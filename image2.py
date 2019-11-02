5758fbf7400c1a735ee4f7db88af4b7861e135a6
Ekta <ektaarora3501@gmail.com>
2019-11-02 22:21:18 +0530

    Creating real time edge detector using Canny edge detection

--- image2.py
+++ image2.py
@@ -12,12 +12,14 @@ while True:
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     #out.write(frame)
 
+    blur=cv2.GaussianBlur(gray,(5,5),0)
+    edged=cv2.Canny(blur,50,200)
     cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
+    cv2.imshow('edged',edged)
 
     if cv2.waitKey(1) & 0xFF== ord('q'):
         break
 
 cap.release()
-3out.release()
+#out.release()
 cv2.destroyAllWindows()
